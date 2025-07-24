33ff1c44c0f594af3f788465a0b58f5ec5b57ef5
Hendri Am <hendriam18@gmail.com>
2025-07-24 13:49:16 +0700

    refactor: change sidebar title

--- resources/views/app/sidebar.blade.php
+++ resources/views/app/sidebar.blade.php
@@ -3,7 +3,7 @@
 	<!-- Brand Logo -->
 	<a href="{{ route('dashboard') }}" class="brand-link bg-dark">
 		<img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: 0.8" />
-		<span class="brand-text font-weight-light">{{ config('app.name', 'Laravel') }}</span>
+		<span class="brand-text font-weight-light">SMPZ</span>
 	</a>
 
 	<!-- Sidebar -->
