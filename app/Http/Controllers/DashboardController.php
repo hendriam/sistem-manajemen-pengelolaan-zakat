754357e3e9c4ba0b31e94beb3c1ca9c8773b9d62
Hendri Am <hendriam18@gmail.com>
2025-07-31 17:39:57 +0700

    feat: add management dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,17 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Muzakki;
+use App\Models\Mustahik;
+use App\Models\ZakatTransaction;
+use App\Models\Distribution;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request) : View
+    public function index(Request $request): View
     {
         $title = "Dashboard";
 
-        return view('dashboard',[
-            'title' => $title,
-        ]);
+        $total_muzakki   = Muzakki::count();
+        $total_mustahik  = Mustahik::count();
+        $total_zakat     = ZakatTransaction::sum('amount');
+        $total_distribusi = Distribution::sum('amount');
+        $saldo_zakat     = $total_zakat - $total_distribusi;
+
+        $pemasukan_per_bulan = ZakatTransaction::select(
+            DB::raw("MONTH(zakat_transaction_date) as bulan"),
+            DB::raw("SUM(amount) as total")
+        )
+            ->groupBy('bulan')
+            ->pluck('total', 'bulan');
+
+        $distribusi_per_bulan = Distribution::select(
+            DB::raw("MONTH(distribution_date) as bulan"),
+            DB::raw("SUM(amount) as total")
+        )
+            ->groupBy('bulan')
+            ->pluck('total', 'bulan');
+
+        $pemasukan_arr = array_fill(1, 12, 0);
+        foreach ($pemasukan_per_bulan as $bulan => $total) {
+            $pemasukan_arr[(int)$bulan] = (int)$total;
+        }
+
+        $distribusi_arr = array_fill(1, 12, 0);
+        foreach ($distribusi_per_bulan as $bulan => $total) {
+            $distribusi_arr[(int)$bulan] = (int)$total;
+        }
+
+        return view('dashboard', compact(
+            'title',
+            'total_muzakki',
+            'total_mustahik',
+            'total_zakat',
+            'total_distribusi',
+            'saldo_zakat',
+            'pemasukan_arr',
+            'distribusi_arr',
+        ));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,38 +2,170 @@
 
 @section('title', $title)
 
-@section('css')
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-@endsection
+@push('css')
+<!-- Ionicons -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+@endpush
 
 <!-- Content Header (Page header) -->
 @section('content')
 <div class="content-wrapper">
-        <!-- Header content -->
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">{{ $title }}</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
+    <!-- Header content -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">{{ $title }}</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{ url('dashboard') }}">{{ $title }}</a></li>
                         <li class="breadcrumb-item active">Starter Page</li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Main content -->
-        <div class="content">
-            <div class="container-fluid">
-                <div class="row">
-                   
+                    </ol>
                 </div>
             </div>
         </div>
     </div>
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{ $total_muzakki }}</h3>
+                            <p>Jumlah Muzakki</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{ $total_mustahik }}</h3>
+                            <p>Jumlah Mustahik</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>Rp {{ number_format($total_zakat, 0, ',', '.') }}</h3>
+                            <p>Zakat Masuk</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>Rp {{ number_format($total_distribusi, 0, ',', '.') }}</h3>
+                            <p>Total Dstribusi</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>Rp {{ number_format($saldo_zakat, 0, ',', '.') }}</h3>
+                            <p>Sisa Saldo</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-6 col-6">
+                    <div class="card">
+                        <div class="card-header bg-light">
+                            <strong>Grafik Pemasukan & Distribusi Zakat (Bulanan)</strong>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="grafikZakat" height="100"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
+
+@push('js')
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+    const ctx = document.getElementById('grafikZakat').getContext('2d');
+
+    const bulanLabels = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun',
+        'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'
+    ];
+
+    const pemasukanZakat = Object.values(@json($pemasukan_arr));
+    const distribusiZakat = Object.values(@json($distribusi_arr));
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: bulanLabels,
+            datasets: [{
+                    label: 'Zakat Masuk',
+                    data: pemasukanZakat,
+                    backgroundColor: 'rgba(54, 162, 235, 0.7)'
+                },
+                {
+                    label: 'Zakat Didistribusikan',
+                    data: distribusiZakat,
+                    backgroundColor: 'rgba(255, 99, 132, 0.7)'
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        callback: function(value) {
+                            return 'Rp ' + value.toLocaleString('id-ID');
+                        }
+                    }
+                }
+            },
+            plugins: {
+                tooltip: {
+                    callbacks: {
+                        label: function(context) {
+                            return context.dataset.label + ': Rp ' + context.parsed.y.toLocaleString('id-ID');
+                        }
+                    }
+                }
+            }
+        }
+    });
+</script>
+@endpush
